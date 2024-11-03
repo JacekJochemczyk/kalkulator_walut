cde2730c51065b12a1c26125a950b6f617e71419
JacekJochemczyk <j.jochemczyk@gmail.com>
2024-11-03 18:52:51 +0100

    pierwszy comit

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace kalkulator_walut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(271, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kwota w PLN";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(271, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wybierz walute docelową";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Wynik przeliczenia:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(271, 274);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(271, 185);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(132, 23);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Przelicz";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Kalkulator_Walut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http; // Klient HTTP do komunikacji z API
+using Newtonsoft.Json; // Biblioteka do konwersji JSON na obiekty C#
+
+namespace kalkulator_walut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("EUR");
+            comboBox1.Items.Add("CHF");
+            comboBox1.Items.Add("GBP");
+        }
+
+        private async Task<Dictionary<string, decimal>> GetExchangeRates()
+        {
+            
+            string url = $"https://open.er-api.com/v6/latest/USD";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<ExchangeRateResponse>(responseBody);
+                return data.Rates;
+            }
+        }
+
+        public class ExchangeRateResponse
+        {
+            public Dictionary<string, decimal> Rates { get; set; }
+        }
+
+        private decimal ConvertAmount(decimal kwotaPLN, decimal rateUSD, decimal rateTarget)
+        {
+            // Krok 1: Przelicz kwotę z PLN na USD
+            decimal kwotaUSD = kwotaPLN / rateUSD;
+
+            // Krok 2: Przelicz kwotę z USD na walutę docelową
+            decimal wynik = kwotaUSD * rateTarget;
+
+            return wynik; // Zwraca wynik przeliczenia
+        }
+
+        private async void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Proszę wybrać walutę docelową.");
+                    return;
+                }
+
+                var rates = await GetExchangeRates(); // Pobierz kursy
+
+                if (rates == null || rates.Count == 0)
+                {
+                    MessageBox.Show("Nie udało się pobrać kursów wymiany.");
+                    return;
+                }
+
+                // Pobierz kwotę w PLN od użytkownika
+                if (!decimal.TryParse(textBox1.Text, out decimal kwotaPLN))
+                {
+                    MessageBox.Show("Proszę wpisać poprawną kwotę w PLN.");
+                    return;
+                }
+
+                string targetCurrency = comboBox1.SelectedItem.ToString(); // Pobierz walutę docelową
+
+                
+                if (rates.TryGetValue("PLN", out decimal ratePLN) && rates.TryGetValue(targetCurrency, out decimal rateTarget))
+                {
+                    // Krok 1: Przelicz kwotę z PLN na USD
+                    decimal kwotaUSD = kwotaPLN / ratePLN; // Teraz przeliczamy z PLN na USD
+
+                    // Krok 2: Przelicz kwotę z USD na walutę docelową
+                    decimal wynik = kwotaUSD * rateTarget; // Przelicz z USD na walutę docelową
+
+                    // Wyświetl wynik w polu tekstowym
+                    textBox2.Text = wynik.ToString("0.00");
+                }
+                else
+                {
+                    MessageBox.Show("Nie udało się znaleźć kursów wymiany.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Wystąpił błąd: " + ex.Message);
+            }
+        }
+    }
+}
